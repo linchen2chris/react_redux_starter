2ef6bac5eeae7dd6c9fd4d6732f1d29a33889894
Chris Lin <lc1990linux@gmail.com>
2016-11-20 23:10:44 +0800

    add middleware to redux

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './Components/App';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const log = store => next => (action) => {
+  console.log(19, store.getState());
+  const result = next(action);
+  return result;
+};
+
+const store = createStore(reducer, {}, applyMiddleware(log));
 
 const app = document.createElement('div');
 document.body.appendChild(app);
