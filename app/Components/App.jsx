08c62b947cb368cc390f4b037246b6e25e041687
Chris Lin <lc1990linux@gmail.com>
2016-11-24 22:19:27 +0800

    [Test]研究一下react ref和DOM操作

--- app/Components/App.jsx
+++ app/Components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import AddTodo from '../Containers/AddTodo';
 import VisibleTodoList from '../Containers/VisibleTodoList';
 import Footer from './Footer';
+import Test from './MyTestComp';
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    <Test />
   </div>
 );
 
