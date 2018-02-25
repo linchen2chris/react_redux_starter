f593c834da58277ad1392f7ea0973c3c9a8d5ece
Chris Lin <lc1990linux@gmail.com>
2018-02-25 20:48:53 +0800

    test autoimport

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import {render} from 'react-dom';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './Components/App';
 import reducer from './reducers';
 import logMiddleware from './logMiddleware';
