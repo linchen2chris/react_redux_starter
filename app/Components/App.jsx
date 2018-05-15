815d612f1d0d2e709d9c8fbfc04adb1beaf7db7d
Chris Lin <lc1990linux@gmail.com>
2018-05-15 10:11:37 +0800

    [chen] add importjs config and refactor

--- app/Components/App.jsx
+++ app/Components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AddTodo from '../Containers/AddTodo';
 import VisibleTodoList from '../Containers/VisibleTodoList';
 import Footer from './Footer';
-import Test from './MyTestComp';
+import MyTestComp from './MyTestComp';
 import InputWrapper from './InputWrapper';
 import MaskInput from './MaskInput';
 
@@ -11,9 +11,9 @@ const App = () => (
     <AddTodo />
     <VisibleTodoList />
     <Footer />
-    <Test />
+    <MyTestComp />
     <br />
-    <Test />
+    <MyTestComp />
     <InputWrapper />
     <MaskInput mask="999 - 999" />
   </div>
